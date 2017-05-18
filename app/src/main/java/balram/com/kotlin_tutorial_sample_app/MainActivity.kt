ce87240c3828921b1559e275e1937d79ab431e91
Balram Pandey <balrampandey19@gmail.com>
2017-05-18 18:26:31 +0530

    configure kotlin in build.gradle

--- app/src/main/java/balram/com/kotlin_tutorial_sample_app/MainActivity.kt
+++ app/src/main/java/balram/com/kotlin_tutorial_sample_app/MainActivity.kt
@@ -3,6 +3,11 @@ package balram.com.kotlin_tutorial_sample_app
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
+/*
+Press shift twice in Android Studio Editor.
+You should be able to see finder, then you should type convert Kotlin and select option*/
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
