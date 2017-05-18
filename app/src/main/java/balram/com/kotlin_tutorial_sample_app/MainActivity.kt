aa016530b76d7079464b44d155c145261679221d
Balram Pandey <balrampandey19@gmail.com>
2017-05-18 18:56:21 +0530

    Kotlin variable and cantrol flow

--- app/src/main/java/balram/com/kotlin_tutorial_sample_app/MainActivity.kt
+++ app/src/main/java/balram/com/kotlin_tutorial_sample_app/MainActivity.kt
@@ -2,6 +2,7 @@ package balram.com.kotlin_tutorial_sample_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 
 /*
@@ -10,8 +11,21 @@ You should be able to see finder, then you should type convert Kotlin and select
 
 class MainActivity : AppCompatActivity() {
 
+    // Kotlin Variable declaration
+
+    var a:Int = 20;
+    var b:Int = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Kotlin Control Flow
+
+        var result = if (a>b) a else b;
+        Log.e("Result",""+result);
+
+
     }
+
+
 }
---
+++ app/src/main/java/balram/com/kotlin_tutorial_sample_app/VariableSample.kt
@@ -0,0 +1,11 @@
+package balram.com.kotlin_tutorial_sample_app
+
+/**
+ * Created by redcarpet on 5/18/17.
+ */
+
+
+class VariableSample constructor(firstname:String)
+   {
+
+   }
